e65653ee833c82c9ccb90ad8aeebeddaca8987f9
Denys Shevchenko <denys.shevchenko@sombrainc.com>
2025-08-29 10:59:49 +0300

    Install Spring Boot, move from Maven to Gradle, add Hello World controller

---
+++ src/main/java/org/shedenys/timestamps/Http.java
@@ -0,0 +1,12 @@
+package org.shedenys.timestamps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Http {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Http.class, args);
+    }
+}
